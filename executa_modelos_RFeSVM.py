77886518a380c983afa258d311d6291a5fc845fd
estevaog3 <eccosta@protonmail.com>
2022-07-08 13:35:16 -0400

    Corrige erro e refatora script executa_modelos_RFeSVM.py
    Ao rodar o script, aparecia um erro dizendo que a variável "clf" não estava definida. Este commit corrige isso e também refatora o código para ficar mais simples, removendo código duplicado

--- executa_modelos_RFeSVM.py
+++ executa_modelos_RFeSVM.py
@@ -22,6 +22,13 @@ def parse_args(argv):
     args = parser.parse_args(argv)
     return args
 
+def get_classifiers(classifier_name):
+    if(classifier_name == 'svm'):
+        return {'svm': svm.SVC()}
+    elif(classifier_name == 'rf'):
+        return {'rf': RandomForestClassifier(random_state = 0)}
+    return {'svm': svm.SVC(), 'rf': RandomForestClassifier(random_state = 0)}
+
 if __name__=="__main__":
     args = parse_args(sys.argv[1:])
 
@@ -34,109 +41,36 @@ if __name__=="__main__":
     X = dataset.iloc[:,:-1] # features
     y = dataset.iloc[:,-1] # class
 
-    if args.classifier == 'svm':
-         clf = svm.SVC()
-    if args.classifier == 'rf':
-         clf = RandomForestClassifier(random_state = 0)
-    print('Fit Model')
-    start_time = timeit.default_timer()
-    clf.fit(X, y)
-    end_time = timeit.default_timer()
-    print("Elapsed Time:", end_time - start_time)
+    classifiers = get_classifiers(args.classifier)
 
-    print('Predict')
-    start_time = timeit.default_timer()
-    pred = clf.predict(X)
-    end_time = timeit.default_timer()
-    print("Elapsed Time:", end_time - start_time)
+    for classifier_name, clf in classifiers.items():
+        print('Fit Model', classifier_name)
+        start_time = timeit.default_timer()
+        clf.fit(X, y)
+        end_time = timeit.default_timer()
+        print("Elapsed Time:", end_time - start_time)
 
+        print('Predict')
+        start_time = timeit.default_timer()
+        pred = clf.predict(X)
+        end_time = timeit.default_timer()
+        print("Elapsed Time:", end_time - start_time)
 
-    tn, fp, fn, tp = confusion_matrix(y, pred).ravel()
-    accuracy = metrics.accuracy_score(y, pred)
-    precision = metrics.precision_score(y, pred, zero_division = 0)
-    recall = metrics.recall_score(y, pred, zero_division = 0)
-    f1_score = metrics.f1_score(y, pred, zero_division = 0)
-    roc_auc = metrics.roc_auc_score(y, pred)
+        accuracy = metrics.accuracy_score(y, pred)
+        precision = metrics.precision_score(y, pred, zero_division = 0)
+        recall = metrics.recall_score(y, pred, zero_division = 0)
+        f1_score = metrics.f1_score(y, pred, zero_division = 0)
+        roc_auc = metrics.roc_auc_score(y, pred)
 
-    precision *= 100.0
-    accuracy *= 100.0
-    recall *= 100.0
-    f1_score *= 100.0
-    roc_auc *= 100.0
+        precision *= 100.0
+        accuracy *= 100.0
+        recall *= 100.0
+        f1_score *= 100.0
+        roc_auc *= 100.0
 
-    data = [{'Accuracy':accuracy,'Precision':precision,'Recall':recall,'F1_Score':f1_score,'RoC_AuC':roc_auc}]
-    df = pd.DataFrame(data)
+        data = [{'Accuracy':accuracy,'Precision':precision,'Recall':recall,'F1_Score':f1_score,'RoC_AuC':roc_auc}]
+        df = pd.DataFrame(data)
 
-    x = (args.dataset).split("/")
-    df.to_csv(args.classifier+ "_output_" + x[-1], index = False)
-    print(df)
-    if args.classifier == 'both': 
-            print("MODELO SVM")
-            clf = svm.SVC()
-            print('Fit Model')
-            start_time = timeit.default_timer()
-            clf.fit(X, y)
-            end_time = timeit.default_timer()
-            print("Elapsed Time:", end_time - start_time)
-
-            print('Predict')
-            start_time = timeit.default_timer()
-            pred = clf.predict(X)
-            end_time = timeit.default_timer()
-            print("Elapsed Time:", end_time - start_time)
-
-
-            tn, fp, fn, tp = confusion_matrix(y, pred).ravel()
-            accuracy = metrics.accuracy_score(y, pred)
-            precision = metrics.precision_score(y, pred, zero_division = 0)
-            recall = metrics.recall_score(y, pred, zero_division = 0)
-            f1_score = metrics.f1_score(y, pred, zero_division = 0)
-            roc_auc = metrics.roc_auc_score(y, pred)
-
-            precision *= 100.0
-            accuracy *= 100.0
-            recall *= 100.0
-            f1_score *= 100.0
-            roc_auc *= 100.0
-
-            data = [{'Accuracy':accuracy,'Precision':precision,'Recall':recall,'F1_Score':f1_score,'RoC_AuC':roc_auc}]
-            df = pd.DataFrame(data)
-
-            df.to_csv('SVM.csv', encoding='utf-8', index=False)
-            print(df)
-
-            print("MODELO RANDOMFOREST")
-
-
-            clf = RandomForestClassifier(random_state = 0)
-            print('Fit Model')
-            start_time = timeit.default_timer()
-            clf.fit(X, y)
-            end_time = timeit.default_timer()
-            print("Elapsed Time:", end_time - start_time)
-
-            print('Predict')
-            start_time = timeit.default_timer()
-            pred = clf.predict(X)
-            end_time = timeit.default_timer()
-            print("Elapsed Time:", end_time - start_time)
-
-
-            tn, fp, fn, tp = confusion_matrix(y, pred).ravel()
-            accuracy = metrics.accuracy_score(y, pred)
-            precision = metrics.precision_score(y, pred, zero_division = 0)
-            recall = metrics.recall_score(y, pred, zero_division = 0)
-            f1_score = metrics.f1_score(y, pred, zero_division = 0)
-            roc_auc = metrics.roc_auc_score(y, pred)
-
-            precision *= 100.0
-            accuracy *= 100.0
-            recall *= 100.0
-            f1_score *= 100.0
-            roc_auc *= 100.0
-
-            data = [{'Accuracy':accuracy,'Precision':precision,'Recall':recall,'F1_Score':f1_score,'RoC_AuC':roc_auc}]
-            df = pd.DataFrame(data)
-
-            df.to_csv('RF.csv', encoding='utf-8', index=False)
-            print(df)
+        x = (args.dataset).split("/")
+        df.to_csv(classifier_name+ "_output_" + x[-1], index = False)
+        print(df)
