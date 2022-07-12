52c7cfba73d1417981905e9a6076c3500a0e4a08
estevaog3 <eccosta@protonmail.com>
2022-07-12 13:44:25 -0400

    Corrige validação cruzada e parâmetros em evaluation.py
    Agora o resultado é um único arquivo csv nomeado a partir da opção de CLI --output-file. O arquivo possui uma coluna para cada métrica e uma coluna indicando o nome do modelo em questão (SVM ou RF)

--- evaluation.py
+++ evaluation.py
@@ -7,16 +7,13 @@ from sklearn.model_selection import cross_validate
 from methods.utils import get_dataset, get_base_parser, get_X_y
 
 def parse_args(argv):
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(parents=[get_base_parser()])
     parser.add_argument(
-        '-d', '--dataset', metavar='str',
-        help='Dataset (csv file).', type=str, required=True)
-    parser.add_argument(
-        '-c', '--classifier', metavar='str',
+        '-m', '--model', metavar='str',
         help="Classifier.",
         choices=['svm', 'rf','both'],
         type=str, default='both')
-    parser.add_argument('--cv', '--k_fold',metavar = 'INT', type = int, default = 10,
+    parser.add_argument('--n_folds', metavar = 'INT', type = int, default = 10,
         help="Number of folds to use in cross validation. Default: 10")
     args = parser.parse_args(argv)
     return args
@@ -37,48 +34,11 @@ def cross_validation(model, X, y, n_folds = 10, metrics=['accuracy', 'precision'
 
 if __name__=="__main__":
     args = parse_args(sys.argv[1:])
+    X, y = get_X_y(args, get_dataset(args))
 
-    try:
-        dataset = pd.read_csv(args.dataset)
-    except BaseException as e:
-        print('Exception: {}'.format(e))
-        exit(1)
-
-    X = dataset.iloc[:,:-1] # features
-    y = dataset.iloc[:,-1] # class
-
-    classifiers = get_classifiers(args.classifier)
+    classifiers = get_classifiers(args.model)
+    results = []
     
-    cross_validation(model = classifiers, _X, _y, _cv = args.k_fold)
-
     for classifier_name, clf in classifiers.items():
-        print('Fit Model', classifier_name)
-        start_time = timeit.default_timer()
-        clf.fit(X,y)
-        end_time = timeit.default_timer()
-        print("Elapsed Time:", end_time - start_time)
-
-        print('Predict')
-        start_time = timeit.default_timer()
-        pred = clf.predict(X)
-        end_time = timeit.default_timer()
-        print("Elapsed Time:", end_time - start_time)
-
-        accuracy = metrics.accuracy_score(y, pred)
-        precision = metrics.precision_score(y, pred, zero_division = 0)
-        recall = metrics.recall_score(y, pred, zero_division = 0)
-        f1_score = metrics.f1_score(y, pred, zero_division = 0)
-        roc_auc = metrics.roc_auc_score(y, pred)
-
-        precision *= 100.0
-        accuracy *= 100.0
-        recall *= 100.0
-        f1_score *= 100.0
-        roc_auc *= 100.0
-
-        data = [{'Accuracy':accuracy,'Precision':precision,'Recall':recall,'F1_Score':f1_score,'RoC_AuC':roc_auc}]
-        df = pd.DataFrame(data)
-
-        x = (args.dataset).split("/")
-        df.to_csv(classifier_name+ "_output_" + x[-1], index = False)
-        print(df)
+        results.append({**cross_validation(clf, X, y, args.n_folds), "model": classifier_name})
+    pd.DataFrame(results).to_csv(args.output_file, index = False)
