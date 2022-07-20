ffe5ef39865ffc585fb3cb6944787f1d95dd211a
estevaog3 <eccosta@protonmail.com>
2022-07-20 11:24:53 -0400

    Corrige run.sh do LR: define OUTPUT_FILE e remove comando echo incorreto

--- methods/LinearRegression/run.sh
+++ methods/LinearRegression/run.sh
@@ -6,10 +6,10 @@ CHECK_PKGS=`pip show ${PKGS[@]} | grep -i -w "not found"`
 [ "$CHECK_PKGS" = "" ] || { echo "instale os pacotes Python: sudo pip install ${PKGS[@]}"; exit; }
 
 [[ $1 && $2 ]] || { echo "Uso: bash $0 OUTPUT_FILE DATASET [DATASET...]" && exit 1;}
-
+OUTPUT_FILE=$1
+shift
 for DATASET in $*
 do
-    echo "python3 -m methods.SigAPI.sigapi_funcoesdeselecao -d $DATASET"
     D_NAME=$(echo $DATASET | awk -F, '{print NF}')
     TS=$(date +%Y%m%d%H%M%S)
     echo "python3 -m methods.LinearRegression.LinearRegression -d $DATASET -o $OUTPUT_FILE"
