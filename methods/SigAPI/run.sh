8930eaf2401003b952d12ad5d4407922077ae086
Sávio Saboia <85359284+SavioSilas@users.noreply.github.com>
2022-08-02 15:52:31 -0400

    Update run.sh

--- methods/SigAPI/run.sh
+++ methods/SigAPI/run.sh
@@ -22,6 +22,6 @@ do
     set_increment `head -1 $DATASET | awk -F, '{print NF-1}'`
     TS=$(date +%Y%m%d%H%M%S)
     OUT_FILENAME="dataset_sigapi_${D_NAME}_$TS"
-    echo "python3 -m methods.SigAPI.main -d $DATASET -o $OUT_FILENAME --output-prefix $OUTPUT_PREFIX -i $INCREMENT"
+    #echo "python3 -m methods.SigAPI.main -d $DATASET -o $OUT_FILENAME --output-prefix $OUTPUT_PREFIX -i $INCREMENT"
     { time python3 -m methods.SigAPI.main -d $DATASET -o $OUT_FILENAME --output-prefix $OUTPUT_PREFIX -i $INCREMENT; } 2> time_$OUT_FILENAME.txt
 done
